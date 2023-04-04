e45cb823071a22b6e2ce1a7c9d935f683b629da4
wlor0623 <34371116+wlor0623@users.noreply.github.com>
2023-04-05 03:35:52 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,13 +1,24 @@
 from flask import Flask, request, jsonify
+
+import subprocess
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-@app.route('/testApi',methods=["GET"])
-def test_api():
+@app.route('/create',methods=["GET"])
+def createVideo():
     print(request)
     id = request.args.get("id");
+    subprocess.call(["python", "/content/SadTalker/inference.py",'--driven_audio', '/content/SadTalker/examples/driven_audio/RD_Radio31_000.wav', '--source_image', '/content/SadTalker/examples/source_image/my_img.jpg', '--result_dir', './my_results'])
+    # subprocess.call(["python", "./video.py",'--driven_audio /content/SadTalker/examples/driven_audio/RD_Radio31_000.wav', '--source_image /content/SadTalker/examples/source_image/my_img.jpg', '--result_dir ./my_results'])
+#     python inference.py \
+#   --driven_audio ./examples/driven_audio/RD_Radio31_000.wav \
+#   --source_image ./examples/source_image/my_img.jpg \
+#   --result_dir ./my_results 
     return{"id":id}
+
 if __name__ == '__main__':
    app.run(host='127.0.0.1',port=7860,debug=True)
