f4025939aa400c90fbdf666bf92a4bc126ad4126
wlor0623 <34371116+wlor0623@users.noreply.github.com>
2023-04-19 23:21:36 +0800

    Update processing.py

--- 1.6/processing.py
+++ 1.6/processing.py
@@ -632,7 +632,7 @@ def process_images_inner(p: StableDiffusionProcessing) -> Processed:
                 print(p.do_not_save_samples)
                 print(p.outpath_samples)
                 print(opts.samples_format)
-                print(p.keys())
+
         
                 outpath_wlor=p.outpath_samples or "outpath/images"
                 print(outpath_wlor)
