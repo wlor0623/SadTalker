017bbb777f78554b47899beb4963f7639db79349
wlor0623 <34371116+wlor0623@users.noreply.github.com>
2023-04-19 18:39:37 +0800

    Update processing.py

--- 1.6/processing.py
+++ 1.6/processing.py
@@ -627,10 +627,11 @@ def process_images_inner(p: StableDiffusionProcessing) -> Processed:
                     image = apply_color_correction(p.color_corrections[i], image)
 
                 image = apply_overlay(image, p.paste_to, i, p.overlay_images)
-                print(p)
-                print(opts)
+               
                 print(opts.samples_save)
                 print(p.do_not_save_samples)
+                print(p.outpath_samples)
+                print(opts.samples_format)
                 if opts.samples_save and not p.do_not_save_samples:
                     fullfn = images.save_image(image, p.outpath_samples, "", seeds[i], prompts[i], opts.samples_format, info=infotext(n, i), p=p)
                     try:
